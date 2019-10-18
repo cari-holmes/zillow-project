15980f749d770a62120eea19f293d2892cbc9311
cariholmes <holmes.l.cari@gmail.com>
2019-10-17 22:21:19 -0500

    split and scale functions

---
+++ split_scale.py
@@ -0,0 +1,50 @@
+import warnings 
+warnings.filterwarnings("ignore")
+import pandas as pd 
+import numpy as np
+from wrangle import wrangle_zillow
+import env 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, QuantileTransformer, PowerTransformer, RobustScaler, MinMaxScaler
+
+
+def split_my_data(df, train_pct=.80, random_state=123):
+    train, test = train_test_split(df, train_size=train_pct, random_state=random_state)
+    return train, test
+
+
+def standard_scaler(train, test):
+    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
+
+
+def scale_inverse(scaler, train_scaler, test_scaler):
+    train_unscaled = pd.DataFrame(scaler.inverse_transform(train_scaled), columns=train_scaled.columns.values).set_index([train.index.values])
+    test_unscaled = pd.DataFrame(scaler.inverse_transform(test_scaled), columns=test_scaled.columns.values).set_index([test.index.values])
+    return scaler, train, test
+
+def uniform_scaler(train, test):
+    scaler = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
+
+def gaussian_scaler(train, test):
+    scaler = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
+
+def min_max_scaler(train, test):
+    scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
+
+def iqr_robust_scaler(train, test):
+    scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
