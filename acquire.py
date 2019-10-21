15f86adb895603848423c4db2d05d8b0b458df60
cariholmes <holmes.l.cari@gmail.com>
2019-10-20 20:12:48 -0500

    Acquire Functions

---
+++ acquire.py
@@ -0,0 +1,43 @@
+import warnings 
+warnings.filterwarnings('ignore')
+import pandas as pd
+import numpy as np
+from util import get_db_url
+
+def get_data_from_sql():
+    query='''
+    SELECT bedroomcnt as bedroom, 
+    bathroomcnt as bathroom, 
+    calculatedfinishedsquarefeet as square_feet,
+    taxamount as tax_amount, 
+    taxvaluedollarcnt as home_value, 
+    proptype.propertylandusedesc as property_type, 
+    fips
+    FROM properties_2017 AS prop
+    JOIN predictions_2017 AS pred USING (parcelid)
+    JOIN propertylandusetype AS proptype USING (propertylandusetypeid)
+    WHERE (transactiondate >= '2017-05-01' AND transactiondate <= '2017-06-30')
+    AND (propertylandusedesc = 'Single Family Residential')
+    AND prop.bedroomcnt > 0
+    AND prop.bathroomcnt > 0
+    AND prop.taxvaluedollarcnt > 0
+    AND prop.lotsizesquarefeet > 0
+    ORDER BY fips;
+    '''
+    df = pd.read_sql(query, get_db_url('zillow'))
+    return df 
+
+
+def clean_data(df):
+    df = df.dropna()
+    df['bedroom'] = df['bedroom'].astype(int)
+    df['fips'] = df['fips'].astype('int')
+    df['home_value'] = df['home_value'].astype(int)
+    df['square_feet'] = df['square_feet'].astype(int)
+
+    return df
+
+def acquire_zillow():
+    df = get_data_from_sql()
+    df = clean_data(df)
+    return df 
